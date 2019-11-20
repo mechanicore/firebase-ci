a1468e3dab89b3d5810d254585805ba4d4bedd51
mechanicore <mechanicoreframe@gmail.com>
2019-11-20 22:49:48 +0800

    Added vuejs project.

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+
+if [ "${GITHUB_API_TOKEN}" ]; then
+  npm run lint
+
+  # Build only from `dev` branch
+  if [ "${TRAVIS_BRANCH}" = 'dev' -a "${TRAVIS_PULL_REQUEST}" = 'false' ]; then
+    npm run build
+    cp README.md dist/
+    cd dist
+    ls -l -a
+  fi
+fi
